b2d70d52089aae86637e664059b065e1fae2c75c
JessyLSC <jessy_luanah@hotmail.com>
2020-08-27 10:20:46 -0300

    atores

--- R/atores.R
+++ R/atores.R
@@ -1,13 +1,18 @@
+
+usethis::use_git()
 library(tidyverse)
 library(dplyr)
 
-
+imdb <- readr::read_rds("data/imdb.rds")
 
 calcular_num_filmes <- function(tab, nome_ator) {
-
   num_filmes <- tab %>%
-    pivot_longer(cols = ator_1, ator_2, ator_3, names_to = "ator") %>%
-    count(nome_ator)
-
-    num_filmes
+    pivot_longer(
+      cols = starts_with("ator"),
+      names_to = "posicao",
+      values_to = "ator") %>%
+    filter(ator == nome_ator) %>%
+  view()
 }
+
+calcular_num_filmes(imdb, "Will Smith")
